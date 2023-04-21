7dfc9c07ab1566e3e85a33ffcdb879c0154a028c
Rene van Dinter <info@gambit24.de>
2023-04-21 09:13:04 +0200

    change api routing

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -6,13 +6,14 @@ export default async function handler(request, response) {
   await dbConnect();
   const { id } = request.query;
 
-  if (!id) {
-    return;
-  }
-
 
   if (request.method === "GET") {
     const place = await Place.findById(id)
+
+    if (!place) {
+      return response.status(404).json({ status: 'Not found' });
+    }
+
     response.status(200).json(place);
   }
 
@@ -27,16 +28,10 @@ export default async function handler(request, response) {
   if (request.method === "DELETE") {
     try {
       await Place.findByIdAndDelete(id);
-
       return response.status(200).json({ status: "Place is deleted" });
     } catch (error) {
       return response.status(400).json({ status: error.message });
     }
   }
-
-  if (!place) {
-    return response.status(404).json({ status: 'Not found' });
-  }
-
 
 }
